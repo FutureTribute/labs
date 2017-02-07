993dfb4ee1fd960518789db944d213cbda8c3796
FutureTribute <bogdan641999@gmail.com>
2017-02-08 01:12:22 +0200

    2

--- lab6.12.py
+++ lab6.12.py
@@ -1,11 +1,17 @@
 #Засько Б., КНИТ16-А
 #Значение переменной x, обозначающее некоторую длину в единицах p, заменить величиной этой же длины в метрах
 
+from enum import Enum
 class measure(Enum):
-    decimetre = 1
-    kilometre = 2
-    metre = 3
-    millimetre = 4
-    centimetre = 5
-x = float(input("length: "))
-p = measure[input("measure: ")]
+    decimetre = 0.1
+    kilometre = 1000
+    metre = 1
+    millimetre = 0.001
+    centimetre = 0.01
+
+try:
+    x = float(input("length: "))
+    p = measure[input("measure: ")]
+    print("Значение в метрах: ", x*p.value)
+except ValueError:
+    print("Длина должна быть числом")
