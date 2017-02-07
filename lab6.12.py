ac6bc3bff69bf4abc39ff152f47aa51b0bb03c9f
FutureTribute <bogdan641999@gmail.com>
2017-02-08 01:18:43 +0200

    3

--- lab6.12.py
+++ lab6.12.py
@@ -8,10 +8,16 @@ class measure(Enum):
     metre = 1
     millimetre = 0.001
     centimetre = 0.01
-
-try:
-    x = float(input("length: "))
-    p = measure[input("measure: ")]
-    print("Значение в метрах: ", x*p.value)
-except ValueError:
-    print("Длина должна быть числом")
+while True:
+    try:
+        x = float(input("length: "))
+        p = measure[input("measure: ")]
+        print("Значение в метрах: ", x*p.value)
+    except (ValueError, KeyError):
+        print("Неверно введенное значение или ключ")
+    cont = input("Для продолжения введите yes, для завершения любое другое значение \n")
+    if cont == "yes":
+        print("")
+        continue
+    else:
+        break
