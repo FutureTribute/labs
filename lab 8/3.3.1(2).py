51d6eaffcd9378168503855bb5d95913b9924b24
FutureTribute <bogdan641999@gmail.com>
2017-03-26 21:17:18 +0300

    now it work correctly

--- lab 8/3.3.1(2).py
+++ lab 8/3.3.1(2).py
@@ -8,15 +8,16 @@ x = int(input("Количество элементов массива: "))
 a = np.arange(x)
 s = int(input("Иcкомый элемент: "))
 i = 0
-j = len(a)-1
-m = int(j/2)
-while a[m] != s and i < j:
+j = len(a) - 1
+while i <= j:
+    m = int((i + j) / 2)
     if s > a[m]:
         i = m+1
-    else:
+    elif s < a[m]:
         j = m-1
-    m = int((i+j)/2)
+    else:
+        break
 if i > j:
     print("Элемент не найден")
 else:
-    print("Элемент найден в позиции", m+1)
+    print("Элемент найден в позиции", m)
