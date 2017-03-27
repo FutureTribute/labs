044b7da334051f773b7601a5841b1f6fb09dfb1e
FutureTribute <bogdan641999@gmail.com>
2017-03-27 13:18:15 +0300

    now it work correctly

---
+++ lab 8/3.3.2(1).py
@@ -0,0 +1,32 @@
+# Засько Б., КНИТ16-А
+# Алгоритм Кнута-Морриса-Пратта
+
+from timeit import timeit
+import numpy as np
+
+S = input("Введите строку: ")
+subS = input("Введите подстроку: ")
+
+t = [0] * len(subS)
+j = 0
+for i in range(1, len(subS)):
+    while j > 0 and subS[i] != subS[j]:
+        j = t[j-1]
+    if subS[i] == subS[j]:
+        j += 1
+    t[i] = j
+
+m = k = 0
+while k < len(S) and m < len(subS):
+    if subS[m] == S[k]:
+        m += 1
+        k += 1
+    elif m == 0:
+        k += 1
+    else:
+        m = t[m - 1]
+else:
+    if m == len(subS):
+        print("Элемент найден в позиции", k - m)
+    else:
+        print("Элемент не найден")
