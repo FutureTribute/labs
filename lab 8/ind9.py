645bacf8a0ef8970cedff5593c482054ff5abca9
FutureTribute <bogdan641999@gmail.com>
2017-03-30 06:12:20 +0300

    НАКАНЕЦТА

--- lab 8/ind9.py
+++ lab 8/ind9.py
@@ -10,12 +10,12 @@ import numpy as np
 
 
 class Shops(Enum):
-    shop1 = [111, "No", "Оболонь"]
-    shop2 = [112, "Yes", "Дарница"]
-    shop3 = [113, "No", "Троещина"]
-    shop4 = [114, "Yes", "Оболонь"]
-    shop5 = [115, "Yes", "Оболонь"]
-    shop6 = [116, "No", "Дарница"]
+    n111 = ["No", "Оболонь"]
+    n112 = ["Yes", "Дарница"]
+    n113 = ["No", "Троещина"]
+    n114 = ["Yes", "Оболонь"]
+    n115 = ["Yes", "Оболонь"]
+    n116 = ["No", "Дарница"]
 
 
 class Items(Enum):
@@ -26,22 +26,22 @@ class Items(Enum):
 
 
 class Requests(Enum):
-    r7 = [114, "v1", 3]
-    r21 = [116, "f1", 8]
-    r70 = [111, "f2", 12]
-    r135 = [113, "f1", 60]
-    r222 = [112, "v1", 9]
-    r500 = [115, "v2", 23]
-    r501 = [111, "f1", 42]
-    r634 = [113, "f1", 34]
-    r789 = [116, "f2", 24]
+    r7 = ["n114", "v1", 3]
+    r21 = ["n116", "f1", 8]
+    r70 = ["n111", "f2", 12]
+    r135 = ["n113", "f1", 60]
+    r222 = ["n112", "v1", 9]
+    r500 = ["n115", "v2", 23]
+    r501 = ["n111", "f1", 42]
+    r634 = ["n113", "f1", 34]
+    r789 = ["n116", "f2", 24]
 
 y = list()
 for i in Items.__members__.values():
     y.append(i.value)
 z = set()
 for i in Shops.__members__.values():
-    z.add(i.value[2])
+    z.add(i.value[1])
 z = list(z)
 
 while True:
@@ -65,23 +65,27 @@ while True:
             else:
                 countY = 0
                 countN = 0
-                for k in Requests.__members__.values():
-                    for j in Items.__members__.values():
-                        for i in Shops.__members__.values():
-                            if x2 != "all":
-                                    if i.value[2] == x2 and j.value == x:
-                                        if i[k.value[0]].value[1] == "No":
+                for j in Items.__members__.values():
+                    if j.value == x:
+                        for k in Requests.__members__.values():
+                            if k.value[1] == Items(x).name:
+                                if x2 != "all":
+                                    if Shops[k.value[0]].value[1] == x2:
+                                        if Shops[k.value[0]].value[0] == "No":
                                             countN += k.value[2]
                                         else:
                                             countY += k.value[2]
-                            else:
-                                if j.value == x:
-                                    if i[k.value[0]].value[1] == "No":
+                                else:
+                                    if Shops[k.value[0]].value[0] == "No":
                                         countN += k.value[2]
                                     else:
                                         countY += k.value[2]
-                a = np.array(["Для тары типа 'Да'", "Для тары типа 'Нет'", "Всего"], [countY, countN, countY + countN])
-                print("Заявки на {} по {}".format(x, x2))
+                count = "{:^5}".format(str(countN + countY) + " кг.")
+                countN = "{:^19}".format(str(countN) + " кг.")
+                countY = "{:^18}".format(str(countY) + " кг.")
+                a = np.array([['Для тары типа "Да"', 'Для тары типа "Нет"', 'Всего'],
+                             [countY, countN, count]])
+                print("\nЗаявки на {} по {}".format(x, x2))
                 print(a)
                 break
     cont = input("Для продолжения введите yes, для завершения любое другое значение \n")
