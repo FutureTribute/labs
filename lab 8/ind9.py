cf2cd42b535f51871cbb37d17d4f3763f2192621
FutureTribute <bogdan641999@gmail.com>
2017-03-30 05:09:35 +0300

    ...

--- lab 8/ind9.py
+++ lab 8/ind9.py
@@ -39,6 +39,10 @@ class Requests(Enum):
 y = list()
 for i in Items.__members__.values():
     y.append(i.value)
+z = set()
+for i in Shops.__members__.values():
+    z.add(i.value[2])
+z = list(z)
 
 while True:
     x = input("Введите наименование интересуемого товара (для получения списка товаров введите list):\n")
@@ -46,16 +50,40 @@ while True:
         print(y)
         continue
     elif x not in y:
-        print("Введено неверное наименование товара")
+        print("Введено неверное наименование товара\n")
         continue
     else:
         while True:
-            x2 = input("Для вывода информации по раёнам введите dis, по всему городу - all:\n")
-            if x2 != ("dis" or "all"):
+            x2 = input("\nДля вывода информации по раёнам введите его название, по всему городу - all\n"
+                       "(для получения списка раёнов введите list2): ")
+            if x2 == "list2":
+                print(z)
+                continue
+            elif (x2 != "all") and (x2 not in z):
                 print("Введено некоректное значение")
                 continue
             else:
-                pass
+                countY = 0
+                countN = 0
+                for k in Requests.__members__.values():
+                    for j in Items.__members__.values():
+                        for i in Shops.__members__.values():
+                            if x2 != "all":
+                                    if i.value[2] == x2 and j.value == x:
+                                        if i[k.value[0]].value[1] == "No":
+                                            countN += k.value[2]
+                                        else:
+                                            countY += k.value[2]
+                            else:
+                                if j.value == x:
+                                    if i[k.value[0]].value[1] == "No":
+                                        countN += k.value[2]
+                                    else:
+                                        countY += k.value[2]
+                a = np.array(["Для тары типа 'Да'", "Для тары типа 'Нет'", "Всего"], [countY, countN, countY + countN])
+                print("Заявки на {} по {}".format(x, x2))
+                print(a)
+                break
     cont = input("Для продолжения введите yes, для завершения любое другое значение \n")
     if cont == "yes":
         print("")
