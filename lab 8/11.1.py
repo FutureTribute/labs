fdf94c7b7282b5911d0a645920a68dc13ba41d17
FutureTribute <bogdan641999@gmail.com>
2017-04-03 03:18:03 +0300

    ...

--- lab 8/11.1.py
+++ lab 8/11.1.py
@@ -3,6 +3,6 @@
 
 import numpy as np
 
-a = np.arange(9).reshape((3, 3))
+a = np.arange(16).reshape((4, 4))
 print("Квадратный массив - ", a)
-print("Повернутый квадратный массив - ", np.rot90(a, -1))
+print("Повернутый квадратный массив - ", np.array(list(zip(*a[::-1]))))
