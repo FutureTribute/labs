fdb3f90ac3c62d78993523d6aaa8d9f274cb160e
FutureTribute <bogdan641999@gmail.com>
2017-04-03 13:15:17 +0300

    ...

--- lab 8/11.1.py
+++ lab 8/11.1.py
@@ -3,6 +3,6 @@
 
 import numpy as np
 
-a = np.arange(16).reshape((4, 4))
+a = np.arange(9).reshape((3, 3))
 print("Квадратный массив - ", a)
 print("Повернутый квадратный массив - ", np.array(list(zip(*a[::-1]))))
--- lab 8/11.3.py
+++ lab 8/11.3.py
@@ -7,8 +7,21 @@ s1 = int(input("Количество строк первой матрицы: "))
 s2 = int(input("Количество столбцов первой матрицы: "))
 c1 = int(input("Количество строк второй матрицы: "))
 c2 = int(input("Количество столбцов второй матрицы: "))
-a = np.matrix(np.arange(s1*s2).reshape((s1, s2)))
-b = np.matrix(np.arange(c1*c2).reshape((c1, c2)))
-print("Первая матрица - ", a)
-print("Вторая матрица - ", b)
-print("Результат произведения матриц - ", np.dot(a, b))
+if s2 == c1:
+    a = np.arange(s1*s2).reshape((s1, s2))
+    b = np.arange(c1*c2).reshape((c1, c2))
+    print("Первая матрица - \n", a)
+    print("Вторая матрица - \n", b)
+    C = np.zeros((s1, c2), dtype=int)
+    zx = list(zip(*b))
+    for i in range(s1):
+        for j in range(c2):
+            z1 = list(a[i])
+            z2 = zx[j]
+            k = 0
+            while k < s2 or k < c1:
+                C[i][j] += z1[k] * z2[k]
+                k += 1
+    print("Результат произведения матриц - \n", C)
+else:
+    print("Умножение матриц при заданых условиях невозможно")
